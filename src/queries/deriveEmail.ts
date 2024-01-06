32299db7f68de6446fa0a367bad4ae78de7a0d7f
samory-ka <samory.ka@doctolib.com>
2024-01-06 15:35:45 +0100

    isoalte fetch logic

---
+++ src/queries/deriveEmail.ts
@@ -0,0 +1,18 @@
+export interface EmailResponse {
+    email: string
+    error: string
+}
+
+export const deriveEmail = (fullName: string, companyDomain: string) => {
+    return fetch(
+        "http://localhost:3001/derive",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({full_name: fullName, company_domain: companyDomain})
+        })
+        .then(response => response.json())
+        .then((emailResponse: EmailResponse) => emailResponse)
+}
