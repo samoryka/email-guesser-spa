50cbb38dd63482455d5c0b4189c4723eed351192
samory-ka <samory.ka@doctolib.com>
2024-01-06 16:18:07 +0100

    extract header

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {deriveEmail} from "./queries/deriveEmail";
 import {TextInput} from "./components/TextInput";
 import {SubmitButton} from "./components/SubmitButton";
 import {ResponseCard} from "./components/ResponseCard";
+import {Header} from "./components/Header";
 
 interface EmailResponse {
     email: string
@@ -27,14 +28,19 @@ export const App = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <TextInput id={"full_name"} label={"Full Name"} value={fullName} disabled={isLoading}
-                       setValue={setFullName}/>
-            <TextInput id={"company_domain"} label={"Company Domain"} value={companyDomain} disabled={isLoading}
-                       setValue={setCompanyDomain}/>
-            <SubmitButton disabled={isLoading}/>
-            {emailResponse?.email && <ResponseCard text={emailResponse.email} isError={false}/>}
-            {emailResponse?.error && <ResponseCard text={emailResponse.error} isError={true}/>}
-        </form>
+        <>
+            <Header/>
+            <main>
+                <form onSubmit={handleSubmit}>
+                    <TextInput id={"full_name"} label={"Full Name"} value={fullName} disabled={isLoading}
+                               setValue={setFullName}/>
+                    <TextInput id={"company_domain"} label={"Company Domain"} value={companyDomain} disabled={isLoading}
+                               setValue={setCompanyDomain}/>
+                    <SubmitButton disabled={isLoading}/>
+                    {emailResponse?.email && <ResponseCard text={emailResponse.email} isError={false}/>}
+                    {emailResponse?.error && <ResponseCard text={emailResponse.error} isError={true}/>}
+                </form>
+            </main>
+        </>
     )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,2 @@
+
+export const Header = () =><header className={"flex items-center w-full p-1 bg-orange-600 text-2xl text-white font-bold"}><h1>Email Guesser</h1></header>
