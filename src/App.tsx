51c02a22f7d01a456c616bcab4d04b5b12e41a1a
samory-ka <samory.ka@doctolib.com>
2024-01-06 16:24:31 +0100

    extract EmailForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React, {FormEvent, useState} from 'react';
-import {deriveEmail} from "./queries/deriveEmail";
-import {TextInput} from "./components/TextInput";
-import {SubmitButton} from "./components/SubmitButton";
+import React, {useState} from 'react';
 import {ResponseCard} from "./components/ResponseCard";
 import {Header} from "./components/Header";
+import {EmailForm} from "./components/EmailForm";
 
 interface EmailResponse {
     email: string
@@ -11,35 +9,15 @@ interface EmailResponse {
 }
 
 export const App = () => {
-    const [fullName, setFullName] = useState("")
-    const [companyDomain, setCompanyDomain] = useState("")
-    const [isLoading, setIsLoading] = useState(false)
     const [emailResponse, setEmailResponse] = useState<EmailResponse>()
-
-    const handleSubmit = async (event: FormEvent) => {
-        event.preventDefault()
-
-        setIsLoading(true)
-
-        const emailResponse = await deriveEmail(fullName, companyDomain)
-        setEmailResponse(emailResponse)
-
-        setIsLoading(false)
-    }
 
     return (
         <>
             <Header/>
             <main>
-                <form onSubmit={handleSubmit}>
-                    <TextInput id={"full_name"} label={"Full Name"} value={fullName} disabled={isLoading}
-                               setValue={setFullName}/>
-                    <TextInput id={"company_domain"} label={"Company Domain"} value={companyDomain} disabled={isLoading}
-                               setValue={setCompanyDomain}/>
-                    <SubmitButton disabled={isLoading}/>
-                    {emailResponse?.email && <ResponseCard text={emailResponse.email} isError={false}/>}
-                    {emailResponse?.error && <ResponseCard text={emailResponse.error} isError={true}/>}
-                </form>
+                <EmailForm setEmailResponse={setEmailResponse}/>
+                {emailResponse?.email && <ResponseCard text={emailResponse.email} isError={false}/>}
+                {emailResponse?.error && <ResponseCard text={emailResponse.error} isError={true}/>}
             </main>
         </>
     )
---
+++ src/components/EmailForm.tsx
@@ -0,0 +1,33 @@
+import {TextInput} from "./TextInput";
+import {SubmitButton} from "./SubmitButton";
+import React, {FormEvent, useState} from "react";
+import {deriveEmail, EmailResponse} from "../queries/deriveEmail";
+
+interface EmailFormProps {
+    setEmailResponse: (emailResponse: EmailResponse) => void
+}
+
+export const EmailForm = ({setEmailResponse}: EmailFormProps) => {
+    const [fullName, setFullName] = useState("")
+    const [companyDomain, setCompanyDomain] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault()
+
+        setIsLoading(true)
+
+        const emailResponse = await deriveEmail(fullName, companyDomain)
+        setEmailResponse(emailResponse)
+
+        setIsLoading(false)
+    }
+
+    return (<form onSubmit={handleSubmit}>
+        <TextInput id={"full_name"} label={"Full Name"} value={fullName} disabled={isLoading}
+                   setValue={setFullName}/>
+        <TextInput id={"company_domain"} label={"Company Domain"} value={companyDomain} disabled={isLoading}
+                   setValue={setCompanyDomain}/>
+        <SubmitButton disabled={isLoading}/>
+    </form>)
+}
